8f2935f95520910dbd9728672057679dd8b03512
NightWng120 <treverholmes9@gmail.com>
2023-08-24 19:31:58 -0400

    added markdown parsers

--- lua/core/plugin_config/treesitter.lua
+++ lua/core/plugin_config/treesitter.lua
@@ -1,9 +1,9 @@
 require'nvim-treesitter.configs'.setup {
 	-- A list of parser namse, or "all"
-	ensure_installed = { "c", "cpp", "python", "javascript", "lua", "rust"},
+	ensure_installed = { "c", "cpp", "python", "javascript", "lua", "rust", "markdown", "markdown_inline"},
 	-- Install parsers synchronously (only applied to `ensure_installed`)
 	sync_install = false,
-	auto_install = true,
+	auto_install = false,
 	highlight = {
 		enable = true,
 	},
